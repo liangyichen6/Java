95df50f398d81866dfc88570153810562305305d
Liangyi Chen <liangyi.chen@pwc.com>
2017-09-21 15:18:16 +0800

    init project

---
+++ chanofresponsibility/src/main/java/ivan/java/design/practice/chanofresponsibility/App.java
@@ -0,0 +1,13 @@
+package ivan.java.design.practice.chanofresponsibility;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
